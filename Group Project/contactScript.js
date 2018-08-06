552ef79b411692b3b7e6c637d4965296d63b740c
zhd1 <zhd1@users.noreply.github.com>
2018-08-06 01:12:07 -0500

    update

--- Group Project/contactScript.js
+++ Group Project/contactScript.js
@@ -1,11 +1,11 @@
 /*global google*/
 
 function initMap() {
-  // The location of Uluru
+  // The location of UWF
   var UWF = {lat: 30.54706, lng: -87.216833};
-  // The map, centered at Uluru
+  // The map, centered at UWF
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 17, center: UWF});
-  // The marker, positioned at Uluru
+  // The marker, positioned at UWF
   var marker = new google.maps.Marker({position: UWF, map: map});
 }
